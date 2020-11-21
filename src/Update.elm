9d0af9c8d7212d397efe5bc5ba9f61b5acabb1ec
Bogdan Salau <Bogdan.Salau@cloudflight.io>
2020-11-21 20:25:34 +0200

    add update

--- src/Update.elm
+++ src/Update.elm
@@ -1,12 +1,42 @@
 module Update exposing (..)
 
-import Model exposing (Model)
+import List exposing (length)
+import Model exposing (Frame(..), Model, emptyModel)
+import String exposing (toInt)
 type Msg
   = Change String
-
+  | Reset
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Change newContent -> model
-      --{ model | content = newContent }
+    Change newContent -> if length model.frameList < 10
+                           then handleChange model newContent
+                           else model
+    Reset -> emptyModel
+
+
+handleChange: Model -> String -> Model
+handleChange model newContent =
+  case newContent of
+    "x" ->  hitStrike model
+    _ -> handleDigit model newContent
+
+handleDigit: Model -> String -> Model
+handleDigit model strDigit =
+  case toInt strDigit of
+    Just x -> halfFrame model x
+
+    Nothing -> model
+
+hitStrike: Model -> Model
+hitStrike model = Debug.log "STRIKE" { model | frameList = Strike::model.frameList }
+
+hitSpare: Model -> Model
+hitSpare model = model
+
+halfFrame: Model -> Int -> Model
+halfFrame model x = Debug.log "STRIKE" { model | frameList = (HalfFrame x)::model.frameList }
+
+openFrame: Model -> Model
+openFrame model = model
